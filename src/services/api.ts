90f0552265aa32f44ca77a15cbac3fa4ac8d7071
Nicholas Tseng <nicholas966@gmail.com>
2024-09-29 16:35:01 +1000

    read token from cookies

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -6,7 +7,8 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = Cookies.get('user_token');
+
     if (token) config.headers['Authorization'] = `Bearer ${token}`;
 
     return config;
